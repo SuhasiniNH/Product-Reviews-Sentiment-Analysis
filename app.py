a3ccd6aec004d68a296dd9169ca932ab03593652
Suhasini Hegde <suhasininhegde@gmail.com>
2026-08-03 00:21:16 +0530

    Adding  app.py and requirement files for testing and deploying through streamlit.

---
+++ app.py
@@ -0,0 +1,31 @@
+import streamlit as st
+import joblib
+
+st.set_page_config(
+    page_title = "Sentiment-Analyser",
+    layout = "centered"
+)
+
+model = joblib.load("sentiment_model.pkl")
+vectorizer = joblib.load("tfidf_vectorizer.pkl")
+
+st.title("Product Review Sentiment Analyzer")
+st.caption("Enter a product review and see whether the model predicts it as positive or negative.")
+
+text = st.text_area("Enter the text to review", placeholder="e.g. This product exceeded my expectations...")
+
+if st.button("Analyze Sentiment"):
+    if text.strip() == "":
+        st.warning("Please enter the text to review")
+    else:
+        text_vec = vectorizer.transform([text])
+        y_predict = model.predict(text_vec)[0]
+        proba = model.predict_proba(text_vec)
+        print("proba:", proba)
+
+        if y_predict == "positive":
+            st.write("Review is positive")
+        else:
+            st.write("Review is negative")  
+        
+
